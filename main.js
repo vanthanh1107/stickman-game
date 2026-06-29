6915237734cd4e36757d0693ce042712509ea46e
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-06-29 21:00:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ window.CHARACTER_REGISTRY = [
     { id: "satthu", className: "Assasin", avatarUrl: "https://api.dicebear.com/7.x/adventurer/png?seed=satthu&backgroundColor=ffdfbf" },
     { id: "phapsu", className: "Winzard", avatarUrl: "https://api.dicebear.com/7.x/adventurer/png?seed=phapsu&backgroundColor=ffdfbf" },
     { id: "hove", className: "Guard", avatarUrl: "https://api.dicebear.com/7.x/adventurer/png?seed=hove&backgroundColor=ffdfbf" },
-    { id: "thichkhach", className: "Slayer", avatarUrl: "https://api.dicebear.com/7.x/adventurer/png?seed=thichkhach&backgroundColor=ffdfbf" }
+    { id: "thichkhach", className: "Slayer", avatarUrl: "https://i.ibb.co/Xd26hLd/b8de0710-bed2-45f6-b258-a722729c3dfb.jpg" }
 ];
 
 window.loadedCharacters = {}; 
